4f79042524baa4cb99205bbedd9ae38a41c9bfae
sethvincent <sethvincent@gmail.com>
2014-09-07 15:57:04 -0700

    fix indents

--- server.js
+++ server.js
@@ -8,33 +8,33 @@ var Handlebars = require('handlebars');
 var hbsLayouts = require('handlebars-layouts')(Handlebars);
 
 var wiki = {
-	name: 'Seattle LocalWiki',
-	url: 'http://localwiki.net/seattle',
+  name: 'Seattle LocalWiki',
+  url: 'http://localwiki.net/seattle',
   api: 'http://localwiki.net/api/v4/'
 };
 
 Handlebars.registerPartial('layout', fs.readFileSync('views/layout.html').toString());
 
 var templates = {
-	index: getView('index'),
-	page: getView('page')
+  index: getView('index'),
+  page: getView('page')
 };
 
 var port = process.env.PORT || 3000;
-var router = 	new director.http.Router();
+var router =   new director.http.Router();
 var staticFiles = st({ path: __dirname + '/static', url: '/static', passthrough: true });
 
 var server = http.createServer(function(req, res){
 
-	/*
-	* if the request is for a static file, handle it here
-	*/
-	if (staticFiles(req, res)) return;
+  /*
+  * if the request is for a static file, handle it here
+  */
+  if (staticFiles(req, res)) return;
 
-	/*
-	* otherwise, let the router handle the request
-	*/
-	router.dispatch(req, res, function(err){
+  /*
+  * otherwise, let the router handle the request
+  */
+  router.dispatch(req, res, function(err){
     if (err) {
       res.writeHead(404);
       res.end();
@@ -43,27 +43,27 @@ var server = http.createServer(function(req, res){
 });
 
 router.get('/', function(){
-	var html = templates.index({ wiki: wiki });
+  var html = templates.index({ wiki: wiki });
   this.res.writeHead(200, { 'Content-Type': 'text/html' });
-	this.res.end(html);
+  this.res.end(html);
 });
 
 router.get('/:id', function(id){
-	var self = this;
-	var pagesRequest = request(wiki.api + '/pages/?region__slug=seattle&tags=' + id + '&format=json');
+  var self = this;
+  var pagesRequest = request(wiki.api + '/pages/?region__slug=seattle&tags=' + id + '&format=json');
 
-	pagesRequest
-		.pipe(wait(function(err, data){
+  pagesRequest
+    .pipe(wait(function(err, data){
 
-			var html = templates.page({
-				wiki: wiki,
-				tag: id,
-				pages: JSON.parse(data).results
-			});
+      var html = templates.page({
+        wiki: wiki,
+        tag: id,
+        pages: JSON.parse(data).results
+      });
 
-		  self.res.writeHead(200, { 'Content-Type': 'text/html' });
-			self.res.end(html);
-		}));
+      self.res.writeHead(200, { 'Content-Type': 'text/html' });
+      self.res.end(html);
+    }));
 });
 
 server.listen(port);
@@ -73,5 +73,5 @@ console.log('app running on http://127.0.0.1:' + port);
 * helper function for pulling in a handlebars template
 */
 function getView(file){
-	return Handlebars.compile(fs.readFileSync('./views/' + file + '.html').toString());
+  return Handlebars.compile(fs.readFileSync('./views/' + file + '.html').toString());
 }
